93c42d47116fd90c851eed0b1279a3cabefdf06d
Cristian Consonni <cristian@balist.es>
2018-03-12 11:25:12 +0100

    Add dependencies, add logging

--- clusters_evolution.py
+++ clusters_evolution.py
@@ -13,6 +13,39 @@ optional arguments:
 
 """
 
+import os
+import re
+import csv
+import json
+import argparse
+import logging
+from typing import Iterable, Iterator, Mapping, NamedTuple, Optional
+
+
+########## logging
+# create logger with 'spam_application'
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.DEBUG)
+
+# create file handler which logs even debug messages
+fh = logging.FileHandler('louvain_clusters.log')
+fh.setLevel(logging.DEBUG)
+
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+##########
+
+
 def main():
 
     cl_date_prev = None
